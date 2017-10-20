8dcc10bba697961cd5f5272e8916098fccc7dcc1
Bob <coderSkin@gmail.com>
2017-10-20 15:42:07 +0800

    添加gulp管理项目

---
+++ gulpfile.js
@@ -0,0 +1,119 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp        = require('gulp'), //本地安装gulp所用到的地方
+    // rjs         = require('requirejs'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    cache       = require('gulp-cache'),
+    minifycss   = require('gulp-minify-css'),
+    uglify      = require('gulp-uglify');
+    sass        = require('gulp-ruby-sass'),
+    clean       = require('gulp-clean'),             //清空文件夹
+    // tiny        = require('tiny-lr'),
+    // server      = tiny(),
+    livereload  = require('gulp-livereload');   //livereload
+
+
+gulp.task('images',function(){
+    var imgDev = './src/images/*.{png,jpg,gif,ico}',
+        imgSrc = './dist/images/';
+    gulp.src(imgDev)
+        .pipe(imagemin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true,  //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true, //类型：Boolean 默认：false 多次优化svg直到完全优化
+            svgoPlugins: [{removeViewBox: false}],//不要移除svg的viewbox属性
+            use: [pngquant()] //使用pngquant深度压缩png图片的imagemin插件
+        }))
+        .pipe(gulp.dest(imgSrc))
+        .pipe(livereload());
+});
+
+
+
+//样式处理
+gulp.task('css',function(){
+    var cssDev = './src/scss/*',
+        cssSrc = './dist/css/';
+    return sass(cssDev)
+        .on('error',sass.logError)
+        .pipe(minifycss())
+        .pipe(gulp.dest(cssSrc))
+        .pipe(livereload());
+})
+
+
+
+//字体输出
+gulp.task('fonts',function(){
+    var fontsDev = './src/fonts/*',
+        fontsSrc = './dist/fonts/';
+    gulp.src(fontsDev)
+        .pipe(gulp.dest(fontsSrc))
+        .pipe(livereload());
+})
+
+
+// HTML处理
+gulp.task('html', function() {
+    var htmlSrc = './src/*.html',
+        htmlDst = './dist/';
+    gulp.src(htmlSrc)
+        .pipe(gulp.dest(htmlDst))
+        .pipe(livereload());
+});
+
+// js处理
+gulp.task('script', function() {
+    gulp.src('./src/lib/*.js')
+        .pipe(gulp.dest('./dist/lib'))
+
+
+    gulp.src('./src/js/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+        .pipe(livereload());
+
+})
+
+//清空图片 样式
+gulp.task('clean',function(){
+    gulp.src(['./dist'],{read: false})
+        .pipe(clean());
+})
+
+// 默认任务 清空图片、样式 gulp
+gulp.task('default', ['clean'], function(){
+    gulp.start('html','script','css','fonts','images');
+});
+
+//默认任务
+
+gulp.task('watch', function () {
+    livereload.listen();
+    //监听图片
+    gulp.watch('./src/images/*',function () {
+        gulp.run('images');
+    });
+
+    //监听css
+    gulp.watch('./src/scss/*',function () {
+        gulp.run('css');
+    })
+
+    //监听js
+    gulp.watch('./src/js/*',function () {
+        gulp.run('javascript');
+    })
+
+    //监听字体文件
+    gulp.watch('./src/fonts/*',function () {
+        gulp.run('fonts');
+    })
+
+    // 监听html
+    gulp.watch('./src/*.html', function(){
+        gulp.run('html');
+    })
+
+});
---
+++ src/js/knaq-table.js
@@ -0,0 +1,119 @@
+/**
+ * Created by Bob on 17/10/19.
+ */
+
+
+(function ($) {
+    $.fn.tableFixed = function(options) {
+        var defaults = {
+            height:'100%',
+            columns:0
+        };
+        var settings = $.extend(defaults, options);
+        return this.each(function(){
+            var $this = $(this),
+                $thead = $this.find('thead'),
+                $tbody = $this.find('tbody'),
+                $colgroup = $this.find('colgroup'),
+                $headWrap  = $('<div class="knaq-t-head"></div>'),
+                $bodyWrap  = $('<div class="knaq-t-body"></div>'),
+                $parent = $this.parent();
+
+            $parent.height(settings.height);
+
+            // 创建thead div
+            var eleColgroup = $colgroup[0].cloneNode(true),
+                $headTable  = $('<table></table>').append( $(eleColgroup) ).append( $thead );
+            $headWrap.append( $headTable ).appendTo( $parent );
+
+            // 设置tbody高度
+            var tbodyHeight = settings.height - parseInt( $headWrap.height() );
+            // 创建tbody div
+            $bodyWrap.append($this).height( tbodyHeight ).appendTo( $parent );
+
+            // 处理ie中滚动条引起的表头对应错位的问题
+            if($bodyWrap[0].offsetWidth !== $bodyWrap[0].clientWidth){
+                $thead.find('tr').each(function(){
+                    var $this = $(this);
+                    $this[0].innerHTML += '<th class="gutter" width="'+ ($bodyWrap[0].offsetWidth - $bodyWrap[0].clientWidth) +'"></th>';
+                });
+            }
+
+            // 设置固定列滚动队列 jquery对象数组
+            var tbodyArr = [];
+
+            // 判断列固定参数
+            if(!!settings.columns && JSON.stringify(settings.columns) !== '{}'){
+                for (var x in settings.columns){
+                    switch (x){
+                        case 'left':
+                        case 'right':
+                            var column = columns(x,settings.columns[x]);
+                            tbodyArr.push(column.find('.knaq-t-body'));
+                            break;
+                        default:
+                            console.log('columns参数属性有误');
+                            break;
+                    }
+                }
+            }
+
+            // 创建固定列
+            function columns(type,n){
+                var $columnsBox = $('<div class="knaq-t-columns"></div>');
+                    if(type === 'left'){
+                        $columnsBox.css({
+                            'left':0
+                        })
+                    }else{
+                        $columnsBox.css({
+                            'right':0
+                        })
+                    }
+
+                var cols = $tbody.find('tr').eq(0).find('td');
+                var columnsWidth = 0;
+                for(var i = 0;i < n; i++){
+                    columnsWidth += cols.eq(i).outerWidth(true);
+                }
+                var $thisInnerHTML = $parent.html();
+                $columnsBox.html($thisInnerHTML).width(columnsWidth);
+
+                if($bodyWrap[0].offsetHeight !== $bodyWrap[0].clientHeight){
+                    var scrollBarWidth = $bodyWrap[0].offsetHeight - $bodyWrap[0].clientHeight;
+                    $columnsBox.css({
+                        'bottom': scrollBarWidth
+                    });
+                    var $columnsBody = $columnsBox.find('.knaq-t-body');
+                    $columnsBody.css({
+                        height: $columnsBody.height() - scrollBarWidth
+                    });
+                };
+
+                $columnsBox.appendTo($parent);
+                // 删除copy的table的id
+                if(!!$this[0].id){
+                    $columnsBox.find('#' + $this[0].id)[0].removeAttribute('id');
+                }
+                return $columnsBox;
+            }
+            // 处理滚动事件
+            var startX = $bodyWrap.scrollLeft(),startY = $bodyWrap.scrollTop();
+                $bodyWrap.on('scroll',function(event){
+                    if(startX !== $bodyWrap.scrollLeft()){
+                        startX = $bodyWrap.scrollLeft();
+                        // 设置表头一起X轴滚动
+                        $headWrap.scrollLeft(startX);
+                    }else if(startY !== $bodyWrap.scrollTop()){
+                        startY = $bodyWrap.scrollTop();
+                        // 设置固定列一起Y轴滚动
+                        tbodyArr.forEach(function(obj){
+                            obj.scrollTop(startY);
+                        })
+                    }else{
+                        return false;
+                    }
+                });
+        });
+    };
+})(jQuery)
